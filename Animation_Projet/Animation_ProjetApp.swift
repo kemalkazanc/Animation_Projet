5cd7a9810b38189e7ca64f759724e4ac93e99fab
Kemal Kazanc <Keemalkazanc@gmail.com>
2024-09-30 10:35:44 +0200

    Initial Commit

---
+++ Animation_Projet/Animation_ProjetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_ProjetApp.swift
+//  Animation_Projet
+//
+//  Created by Kemal Kazanc on 30/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Animation_ProjetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animation_ProjetTests/Animation_ProjetTests.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_ProjetTests.swift
+//  Animation_ProjetTests
+//
+//  Created by Kemal Kazanc on 30/09/2024.
+//
+
+import Testing
+@testable import Animation_Projet
+
+struct Animation_ProjetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
